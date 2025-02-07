e0d9f109b09fed56138997f431d4230b311375af
Aether <2085163736@qq.com>
2025-02-07 08:55:44 +0800

    update files

---
+++ demo/tcpserver.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<arpa/inet.h>
+#include<memory.h>
+
+using namespace std;
+
+int main() {
+    int lfd = socket(AF_INET,SOCK_STREAM,0);
+    sockaddr_in sin;
+    int sin_size = sizeof(sockaddr_in);
+    memset(&sin,0,sin_size);
+    sin.sin_family = AF_INET;
+    sin.sin_port = htons(4567);
+    sin.sin_addr.s_addr = INADDR_ANY;
+    if(bind(lfd,(sockaddr*)&sin,sizeof(sockaddr))!=0) {
+        perror("bind");
+    }
+    listen(lfd,SOMAXCONN);
+    cout<<"waiting"<<endl;
+    int sock = accept(lfd,(sockaddr*)&sin,(socklen_t*)&sin_size);
+    cout<<"connected"<<endl;
+
+    return 0;
+}
